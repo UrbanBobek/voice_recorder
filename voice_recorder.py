d6bac764863ef15306038ff66d2dc67f22fbe3b6
urban <bobek.urban@gmail.com>
2021-01-02 22:11:23 +0100

    keyboard support

--- voice_recorder.py
+++ voice_recorder.py
@@ -1,7 +1,9 @@
 import kivy
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -9,6 +11,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from recorder import Recorder
 
 import threading
+import mute_alsa
 
 Builder.load_file('voicerecorder.kv')
 
@@ -18,45 +21,47 @@ Builder.load_file('voicerecorder.kv')
 
 class StartLayout(Screen):
     pass
-    # recorder = Recorder()
-    # rec = recorder.open("test.wav")
 
-    # def test(self):
-    #     print("Ammm what?")
-
-    # def start_recording(self):
-    #     print("Snemaj!")
-    #     self.rec.start_recording()
-
-    # def stop_recording(self):
-    #     print("NEHI snemat")
-    #     self.rec.stop_recording()
-    # rec = Recorder()
 
 class RecordScreen(Screen):
+    next_rec = ObjectProperty(None)
+    text_to_read = StringProperty()
     # Create Recorder object
     recorder = Recorder()
     rec = None
-
-    # Read user code from file
-    fp = open("user_data/user_data.txt")
-    t = fp.read()
-    t = t.split()
-    code = t[-1]
-    print(code)
-    fp.close()
-
-    with open("user_data/user_data.txt", "a") as fp:
-        fp.write("Imena posnetkov:\n")
-
     text_id = 0
     txt = []
+    
+    # Store text lines to read
     f = open("text/text.txt")
     for i, line in enumerate(f):
         txt.append(line[0:-1])
     f.close()
+    # Set initial text
+    
 
-    text_to_read = "Pritisnite gumb 'Naslednji' (enter/space) in pričnite brati tekst na ekranu"
+    def __init__(self, **kwargs):
+        super(RecordScreen, self).__init__(**kwargs)
+        Window.bind(on_key_down=self._on_keyboard_down)
+        self.text_to_read = "Pritisnite gumb 'Naslednji' (enter/space) in pričnite brati tekst na ekranu"
+
+    def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
+        if  keycode == 40 or keycode == 44 or keycode == 79:  # 40 - Enter key pressed, 44 - spacebar, 79 - right arrow key
+            self.next_recording()
+
+    # Read user code from file
+    def return_user_data(self):
+        with open("temp/curr_user_data.txt") as f:
+            t = f.read()
+            t = t.split()
+            if len(t) > 1:
+                curr_user = t[0]
+                code = t[-1]
+            else:
+                curr_user = "noUser"
+                code = "NoCode"
+            f.close()
+        return curr_user, code
 
     def next_recording(self):
         if self.text_id > len(self.txt):
@@ -67,11 +72,13 @@ class RecordScreen(Screen):
             self.rec.stop_recording()
             self.rec.close()
         
-        file_name = "{}_{}.wav".format(self.code, str(self.text_id).zfill(5))
+        curr_user, code = self.return_user_data()
+        file_name = "{}_{}.wav".format(code, str(self.text_id).zfill(5))
         self.rec = self.recorder.open(str("recordings/"+file_name))
         self.rec.start_recording()
-        with open("user_data/user_data.txt", "a") as self.fp:
-            self.fp.write("['{}', '{}']\n".format(file_name, self.txt[self.text_id]))
+
+        with open("user_data/{}".format(curr_user), "a+") as fp:
+            fp.write("['{}', '{}']\n".format(file_name, self.txt[self.text_id]))
         # print(self.txt[self.text_id])
         self.text_to_read = str(self.txt[self.text_id])
         self.text_id += 1
@@ -98,6 +105,7 @@ class UserDataScreen(Screen):
     def save_user_data(self, name, surname, code, region):
         print("Male: {}   Female {} ".format(self.male, self.female ))
         print("Name: {}   Surname: {}    Code: {}   Region: {}".format(name, surname, code, region))
+        user_data_filename = "{}{}{}.txt".format(name, surname, code)
 
         if self.male is True:
             sex = "m"
@@ -105,7 +113,13 @@ class UserDataScreen(Screen):
             sex = "z"
 
         data = 'Ime: {}\nPriimek: {}\nSpol: {}\nRegija: {}\nKoda: {}\n'.format(name, surname, sex, region, code)
-        f = open("user_data/user_data.txt", "w")
+        f = open("user_data/{}".format(user_data_filename), "w")
+        f.write(data)
+        f.close()
+        
+        # Save who is the current subject to temp folder
+        f = open("temp/curr_user_data.txt", "w")
+        f.write("{}\n".format(user_data_filename))
         f.write(data)
     
 
